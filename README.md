c7a9d796d5472b855d08d4c60ba0f4956f768025
humanG0D <lhulhumaung@gmail.com>
2023-04-08 01:59:15 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# simple-HTTPS-server
+# simple-HTTPS-server.py
+
+There are times when we need to spin up HTTPS server for file sharing and hosting exploits instead of `simpleHTTPserver`.
+
+### Create a self-signed certificate
+
+```bash
+openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+```
+
+You will now have a `server.pem` in your directory.
+
+### Examples:
+
+Make sure to put the path of `server.pem` in the script. Or you can put them in the same directory.
+
+`home/user/www $> python3 simple-HTTPS-server.py`
+
+You can also create a new directory for hosting purpose. In that case:
+
+`home/user/www/web $> python3 ../simple-HTTPS-server.py`
---
+++ simple-HTTPS-server.py
@@ -0,0 +1,20 @@
+import http.server
+import ssl
+
+class CORSRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
+
+class SecureHTTPServer(http.server.HTTPServer):
+    def __init__(self, server_address, HandlerClass):
+        super().__init__(server_address, HandlerClass)
+        self.socket = ssl.wrap_socket(self.socket, certfile='./server.pem', server_side=True)
+
+port = 4443
+server_address = ('0.0.0.0', port)
+httpd = SecureHTTPServer(server_address, CORSRequestHandler)
+print("Serving at port: ", port)
+httpd.serve_forever()
